6ae3b0b687cdb040c9bb259977d3561efefa6667
GSWes <wesley.hedrick@gmail.com>
2020-12-14 07:14:47 -0500

    created all files

---
+++ createExpressApp.sh
@@ -0,0 +1,95 @@
+#!/bin/zsh
+
+echo "What is the name of your project?"
+read ProjectName
+echo "What port will your server be listening on?"
+read PORT
+echo "*******************************"
+echo "*Creating Project $ProjectName*"
+echo "*******************************"
+
+# #Make Directories
+mkdir $ProjectName
+echo $PORT
+mkdir $ProjectName/public $ProjectName/view $ProjectName/models $ProjectName/routes $ProjectName/public/stylesheets
+touch $ProjectName/app.js $ProjectName/readMe.md
+touch $ProjectName/stylesheets/style.css
+
+
+# #Protect node_modules in .gitignore
+echo "***********************************"
+echo "*Adding node_modules to .gitignore*"
+echo "***********************************"
+echo node_modules >> $ProjectName/.gitignore
+
+echo "*******************************"
+echo "********Creating app.js********"
+echo "*******************************"
+
+
+echo "const http = require('http');
+const hostname = '127.0.0.1';
+const express = require('express');
+const morgan = require('morgan');
+const logger = morgan('tiny');
+const app = express();
+app.use(logger);
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+const es6Renderer = require('express-es6-template-engine');
+app.engine('html', es6Renderer);
+app.set('views', 'templates');
+app.set('view engine', 'html');
+
+const server = http.createServer(app);
+app.get('/', (req, res) =>{
+    res.send('Hello, world!')
+});
+
+server.listen($PORT, hostname, () => {
+    console.log('Server running at localhost, port $PORT');
+});
+" >> $ProjectName/app.js
+
+# #Initialize as node package
+cd $ProjectName
+npm init -y
+cd ..
+
+edho "*********************************"
+echo "*********installing morgan*******"
+echo "*********************************"
+
+
+npm install morgan --prefix $ProjectName
+
+echo "*********************************"
+echo "********installing express*******"
+echo "*********************************"
+
+npm install express --prefix $ProjectName
+
+npm install --save-dev nodemon --prefix $ProjectName
+
+echo "*********************************"
+echo "***Installing Standard Packages**"
+echo "*********************************"
+
+
+npm install --prefix $ProjectName
+
+
+echo "You're ready to go. Nodemon has
+been installed, but you'll need to 
+add it to your scripts and dev 
+dependencies in package.json."
+
+
+
+
+
+
+
+
---
+++ readME.md
@@ -0,0 +1,30 @@
+# Express App Generator
+
+## This is a shell script that will scaffold a basic Express app. 
+By 'scaffold,' I mean that it will do the following:
+    - It makes these directories:
+        Project directory based on name you input
+        Views
+        Models
+        Public
+        Routes
+    - It creates these files:
+        app.js
+        - app.js is provisioned with everything needed for basic server function.
+            -  http, express, morgan, nodemon, and all modules that get installed with 
+              'npm install'.
+            - express-es6-template-engine is set up and configured.
+        style.css
+    - It initializes the project folder with npm init -y.
+    - It sets up one route with the response 'Hello, world!'
+
+While there are other helpful express App generators available (Have your mind blown by checking into [this](https://code.visualstudio.com/docs/nodejs/nodejs-tutorial#_an-express-application)), this express app builds all the basic and uses es6-templates instead of pug. 
+
+How to Use It
+1. Download the folder. 
+2. From inside the folder, use your shell run installExpressAppGenerator.sh.
+    Ex: zsh installExpressAppGenerator.sh
+    or
+    Ex: bash installExpressAppGenerator.sh
+3. The install is quick, after which you should be able to create a new Express app anywhere by typing `createExpressApp.sh`. 
+
