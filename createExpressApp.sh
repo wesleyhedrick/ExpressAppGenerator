9327d378b766507d4d1538dbbfba7b4babb41296
GSWes <wesley.hedrick@gmail.com>
2021-01-06 21:36:16 -0500

    fixed various

--- createExpressApp.sh
+++ createExpressApp.sh
@@ -21,6 +21,7 @@ echo "***********************************"
 
 echo node_modules >> $ProjectName/.gitignore
 echo .env >> $ProjectName/.gitignore
+echo .DS_Store >> $ProjectName/.gitignore
 
 echo "Creating .env file"
 
@@ -59,19 +60,14 @@ const es6Renderer = require('express-es6-template-engine');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 const app = express();
-
-
-const logger = morgan('tiny');
+const logger = morgan('dev');
 const hostname = '127.0.0.1';
-
 //Register Middleware
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
 // disabling for local development
 // app.use(helmet());
-
 app.use(session({
     store: new FileStore(),  // no options for now
     secret: process.env.SESSION_SECRET,
@@ -82,16 +78,18 @@ app.use(session({
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }));
-
 app.engine('html', es6Renderer);
 app.set('views', 'templates');
 app.set('view engine', 'html');
-
 const server = http.createServer(app);
 app.get('/', (req, res) =>{
     res.send('Your app is running. Start building!')
 });
 
+//Error Handling for Bad Routes
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
 server.listen($PORT, hostname, () => {
     console.log('Server running at localhost, port $PORT');
 });
@@ -161,7 +159,7 @@ touch $ProjectName/controllers/index.js
 touch $ProjectName/routes/index.js
 mkdir $ProjectName/public/stylesheets
 touch $ProjectName/public/stylesheets/style.css
-$ProjectName/readMe.md
+touch $ProjectName/readMe.md
 
 
 echo "module.exports = {
@@ -192,12 +190,3 @@ echo "To start the app, just navigate to
     and type createExpressApp.sh"
 echo "********************************"
 echo "********************************"
-
-
-
-
-
-
-
-
-
